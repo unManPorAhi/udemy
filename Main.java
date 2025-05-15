d10b98a414f86b4fa1b58488dbd10f019f53beb9
alfeizar <jose_tru_jl@hotmail.com>
2025-05-15 00:56:54 -0500

    Add age check to determine if user is an adult

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        int edad = 30;
+
+        if (edad >= 18) {
+            System.out.println("Eres mayor de edad");
+        } else {
+            System.out.println("Eres menor de edad");
+        }
     }
 }
